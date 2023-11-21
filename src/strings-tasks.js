2d526149a268816edfeacb3e44cb48b8c270e50f
saboheb <100vbo21@gmail.com>
2023-11-21 17:30:31 +0400

    feat: processe 11th task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -195,8 +195,12 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  let sum = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    sum += str.charCodeAt(i);
+  }
+  return sum;
 }
 
 /**
