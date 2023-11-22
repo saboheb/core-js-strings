8437f9d3d4cb5e901072ea60555523ca926c8448
saboheb <100vbo21@gmail.com>
2023-11-22 11:29:51 +0400

    feat: solution 14th task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -249,8 +249,10 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  return `${minutes.toString().padStart(2, '0')}:${seconds
+    .toString()
+    .padStart(2, '0')}`;
 }
 
 /**
