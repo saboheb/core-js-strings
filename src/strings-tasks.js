f1f1a36faed7abef2384ccea8ca136d12a12e59e
saboheb <100vbo21@gmail.com>
2023-11-23 10:32:57 +0400

    feat: solution 22th task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -387,8 +387,11 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  return str
+    .split('')
+    .map((c) => (c === c.toUpperCase() ? c.toLowerCase() : c.toUpperCase()))
+    .join('');
 }
 
 /**
